37e7fcd83fa8979bc020c71f2d28eb1017661ad8
Ankur Mallick <ankurmallick07@gmail.com>
2023-02-07 23:43:38 +0530

    discord bot

---
+++ greetings_cog.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+
+from apikey import *
+
+
+class greetings_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="hello", help="Greets you")
+    async def hello(self, ctx):
+        await ctx.send("Hello! I am Fox bot.")
+
+    @commands.Cog.listener()
+    async def on_member_join(member):
+        jokeurl = "https://joke3.p.rapidapi.com/v1/joke/%7Bid%7D"
+        headers = {
+            "X-RapidAPI-Key": rapidAPI_joke_key,
+            "X-RapidAPI-Host": "joke3.p.rapidapi.com"
+        }
+        response = requests.request("GET", jokeurl, headers=headers)
+        channel = channel.get_channel(1070368972643516490)
+        await channel.send(json.loads(response.text)['content'])
---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from apikey import *
+
+
+# import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+from greetings_cog import greetings_cog
+# BOTTOKEN = botTOKEN
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='.', intents=intents)
+bot.remove_command('help')
+
+# remove the default help command so that we can write out own
+# bot.remove_command('help')
+
+
+# register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+bot.add_cog(greetings_cog(bot))
+
+# start the bot with our token
+bot.run(botTOKEN)
